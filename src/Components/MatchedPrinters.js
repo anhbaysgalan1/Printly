4e5e6ac16a156ac2fdea04595eb34ccca3a5ad1e
Molly Lansford <mollylansford2019@u.northwestern.edu>
2019-02-16 17:46:59 -0600

    Initial match printers page set up

--- src/Components/MatchedPrinters.js
+++ src/Components/MatchedPrinters.js
@@ -11,13 +11,64 @@ var config = {
 firebase.initializeApp(config);
 
 class MatchedPrinters extends Component {
-  render() {
-    return (
-      <div>
-          HELLO
-      </div>
-    );
+  constructor(){
+      super();
+
+      this.state = {
+        active_printers : []
+      };
   }
+
+  componentWillMount = () => {
+    firebase.database().ref('active_printers').on('value', (snapshot) => {
+        let printer_buff = [];
+        snapshot.forEach((child) => {
+            printer_buff.push(child.val());
+        })
+
+        this.setState({
+            active_printers: printer_buff
+        });
+        console.log(printer_buff);
+    });
+  };
+  
+    render() {
+        let printer_data = Object.entries(this.state.active_printers).map(([id, data]) => {
+            return (<PrinterInfo data={data} key={id}></PrinterInfo>);
+        });
+
+        return (
+        <div >
+            HELLO
+            {printer_data}
+        </div>
+        );
+  }
+}
+
+class PrinterInfo extends Component {
+    render(){
+        let stars = [];
+        
+        for (let i = 0; i < this.props.data["rating"]; i++){
+            stars.push(<span className="fa fa-star checked" key={i}></span>)
+        }
+        return (
+        <div className="printer_info">
+            <div className="printer_image">
+                <img src='../logo.svg' className="App_logo" alt="logo" />
+            </div>
+            <div className="printer_data">
+                Name: {this.props.data["name"]} 
+                <br/>
+                Address: {this.props.data["address"]}
+                <br/>
+                Rating: {stars}
+            </div>
+        </div>
+        );
+    }
 }
 
 export default MatchedPrinters;
