18ca8d78e8eff31c4d2bdbdcc52d51bbc40d05e1
Alex Lu Wang <alexluwang2019@u.northwestern.edu>
2019-02-22 11:28:12 -0600

    make title bar scale

--- src/Components/JobInProgress.js
+++ src/Components/JobInProgress.js
@@ -45,7 +45,7 @@ class JobInProgress extends Component {
 		return (
 			<div>
 				<div className="title">
-					Job Status
+					<div className="pagetitle">Job Status</div>
 					<img src='https://firebasestorage.googleapis.com/v0/b/printly.appspot.com/o/logo.png?alt=media&token=d339ba8b-b16f-4c4b-8fce-e56e2ddfdf29' className="logo" alt="logo"/>
 				</div>
 				{this.props.print_options['transfer'] === 'delivery' ? 
