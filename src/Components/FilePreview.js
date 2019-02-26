30b5a875e94544684cffd9dc3a07d9d483e21f5c
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-02-25 21:09:09 -0600

    actually display the new file

--- src/Components/FilePreview.js
+++ src/Components/FilePreview.js
@@ -8,18 +8,14 @@ export default class FilePreview extends Component {
 	render() {
 		if(this.props.file_data && this.props.file_name) {
 			return (
-				<FileViewer
-					key={this.props.file_name.name}
-					filePath={this.props.file_data}
-					fileType={this.props.file_name.name.substring(this.props.file_name.name.lastIndexOf('.')+1)}
-				/>
+				<iframe src="https://firebasestorage.googleapis.com/v0/b/printly.appspot.com/o/preview?alt=media&token=b973789f-1f81-4d74-89b8-d75a2f25441d" width='100%' height='500'></iframe>
 
 			);
 		}
 		else {
 			return (
 				<div>
-					<iframe src="https://firebasestorage.googleapis.com/v0/b/printly.appspot.com/o/hw4.pdf?alt=media&token=b973789f-1f81-4d74-89b8-d75a2f25441d" width='100%' height='500'></iframe>
+					Please select a file.
 				</div>)
 		}
   }
