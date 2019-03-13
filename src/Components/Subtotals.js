5f0a667833ae162d45a9df5a0f2e7da783fbcf01
gthomas2018 <garrettthomas2018@u.northwestern.edu>
2019-03-13 17:55:44 -0500

    Grand Total height fix

--- src/Components/Subtotals.js
+++ src/Components/Subtotals.js
@@ -62,7 +62,7 @@ const styles = ({
 class Subtotals extends React.Component {
 	render() {
 		return (
-			<div style={{ height: '6em', backgroundColor:  '#04619f', float: 'right', width: "15vw", paddingTop: '1em', paddingBottom: '1.5em' }}>
+			<div style={{ height: '5.5em', backgroundColor:  '#04619f', float: 'right', width: "15vw", paddingTop: '1em', paddingBottom: '1.5em' }}>
 				<div style={{ fontWeight: "bold", fontSize: "1.7em", color: "white" }}>Grand Total</div>
 				<div style={{ fontSize: "1.2em", color: "white", textDecoration: "line-through" }}>
 					${(this.props.price + parseFloat(this.props.deliv_fee)).toFixed(2)}
