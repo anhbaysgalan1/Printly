7d26faae22c0157459ef323d5cd93e5d20e3f4b1
gthomas2018 <garrettthomas2018@u.northwestern.edu>
2019-03-13 17:42:28 -0500

    Grand total mat ui is gone

--- src/Components/Subtotals.js
+++ src/Components/Subtotals.js
@@ -61,20 +61,18 @@ const styles = ({
 
 class Subtotals extends React.Component {
 	render() {
-		const { classes } = this.props;
 		return (
-			<div className={classes.root} >
-				<MuiThemeProvider theme={theme}>
-					<List>
-						<ListItem>
-							<ListItemText	primary="Grand Total"
-											secondary={'$' + ((this.props.price + parseFloat(this.props.deliv_fee)) - this.props.discount_rate*(this.props.price + parseFloat(this.props.deliv_fee))).toFixed(2)}/>
-						</ListItem>
-					</List>
-				</MuiThemeProvider>
+			<div style={{ height: '6em', backgroundColor:  '#04619f', float: 'right', width: "15vw", paddingTop: '1em', paddingBottom: '1.5em' }}>
+				<div style={{ fontWeight: "bold", fontSize: "1.7em", color: "white" }}>Grand Total</div>
+				<div style={{ fontSize: "1.2em", color: "white", textDecoration: "line-through" }}>
+					${(this.props.price + parseFloat(this.props.deliv_fee)).toFixed(2)}
+				</div>
+				<div style={{ fontSize: "1.6em", color: "white" }}>
+					${((this.props.price + parseFloat(this.props.deliv_fee)) - this.props.discount_rate*(this.props.price + parseFloat(this.props.deliv_fee))).toFixed(2)}
+				</div>
 			</div>
 		);
 	}
 }
 
-export default withStyles(styles, { withTheme: true })(Subtotals);
+export default Subtotals;
